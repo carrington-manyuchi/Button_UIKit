10ef3847579a911f12f9d2adbfb943ff9b387e66
Carrington <Manyuchi>
2023-07-05 21:39:33 +0200

    programmatic button

--- Button_UIKit/ViewController.swift
+++ Button_UIKit/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let myButton: UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Programmed Button", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.setTitleShadowColor(UIColor.black, for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.layer.shadowOffset = CGSize(width: 3, height: 3)
+        button.layer.shadowColor = UIColor.brown.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.cornerRadius = 30
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 25)
+        
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
+        configureConstraints()
+        
     }
 
+    private func configureViews() {
+        view.addSubview(myButton)
+    }
+    
+    private func configureConstraints() {
+        let myButtonConstraints = [
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            myButton.heightAnchor.constraint(equalToConstant: 65),
+            myButton.widthAnchor.constraint(equalToConstant: 260)
+        ]
+        
+        
+        NSLayoutConstraint.activate(myButtonConstraints)
+    }
 
 }
 
